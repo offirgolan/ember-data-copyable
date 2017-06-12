c22b2f8f974d5fbe3100738f301a582cd859828b
Offir Golan <offirgolan@gmail.com>
2017-06-12 11:53:42 -0700

    Use hasMany and belongsTo relationship object to copy by ref

--- addon/mixins/copyable.js
+++ addon/mixins/copyable.js
@@ -170,7 +170,19 @@ export default Ember.Mixin.create({
 
         // We dont need to yield for a value if it's just copied by ref.
         if (copyByReference.includes(name)) {
-          attrs[name] = this.get(name);
+          let ref = this[meta.kind](name);
+          let copyRef = model[meta.kind](name);
+
+          /*
+            NOTE: This is currently private API but has been approved @igorT.
+                  Supports Ember Data 2.5+
+           */
+          if (meta.kind === 'hasMany') {
+            copyRef.hasManyRelationship.addRecords(ref.hasManyRelationship.members)
+          } else if (meta.kind === 'belongsTo') {
+            copyRef.belongsToRelationship.addRecords(ref.belongsToRelationship.members)
+          }
+
           continue;
         }
 
