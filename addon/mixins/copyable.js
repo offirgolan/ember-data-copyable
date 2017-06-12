f1da78fa1de06f744c5c69141f2d7523468a3e02
Offir Golan <offirgolan@gmail.com>
2017-06-12 13:15:15 -0700

    Use runInDebug for logging

--- addon/mixins/copyable.js
+++ addon/mixins/copyable.js
@@ -8,7 +8,8 @@ const {
   assign,
   Logger,
   guidFor,
-  isEmpty
+  isEmpty,
+  runInDebug
 } = Ember;
 
 const {
@@ -79,7 +80,7 @@ export default Ember.Mixin.create({
 
       return model;
     } catch (e) {
-      Logger.error('[ember-data-copyable]', e);
+      runInDebug(() => Logger.error('[ember-data-copyable]', e));
 
       // Throw so the task promise will be rejected
       throw new Error(e);
@@ -88,7 +89,7 @@ export default Ember.Mixin.create({
         let copiesKeys = keys(_meta.copies);
 
         // Display the error
-        Logger.error(`[ember-data-copyable] Failed to copy model '${this}'. Cleaning up ${copiesKeys.length} created copies...`);
+        runInDebug(() => Logger.error(`[ember-data-copyable] Failed to copy model '${this}'. Cleaning up ${copiesKeys.length} created copies...`));
 
         // Unload all created records
         copiesKeys.forEach((key) => store.unloadRecord(_meta.copies[key]));
